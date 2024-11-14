8564d6ec12576198276ff950c78152d05f03ee72
lambrin <lambrin@DESKTOP-0K6ACJT>
2024-11-14 21:28:11 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft
+{
+    class Player
+    {
+        public string Input = "";
+        public int x = 4;
+        public int y = 11;
+        public int x_size = 70;
+        public int y_size = 34;
+    }
+
+    class cordinates : Player 
+    {
+        public int x = 4;
+        public int y = 4;
+        
+
+    }
+    
+    class Game  
+    {
+        public int x_size = 30;
+        public int y_size = 13;
+
+    }
+
+    class Block_ids
+    {
+         
+    }
+    //▀▄░█
+}
---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+namespace Minecraft
+{
+    class Program
+    {
+
+        protected static int origRow;
+        protected static int origCol;
+
+        protected static void WriteAt(string s, int x, int y)
+        {
+            try
+            {
+                Console.SetCursorPosition(origCol + x, origRow + y);
+
+                Console.Write(s);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Minecraft v0.0.1");
+            Console.ForegroundColor = default;
+
+            Game overworld = new Game();
+            Player player = new Player();
+
+            string[] Blocks = new string[10];
+            {
+                //▀▄░█
+                Blocks[1] = "▀▀"; // grass
+                Blocks[2] = "██"; // dirt
+                Blocks[3] = "██"; // stone
+                Blocks[4] = "██"; // log
+                Blocks[5] = "██"; // leaves
+            }
+            int[,] grid = new int[player.y_size, player.x_size];
+            BuildWorld(grid);
+            while (true)
+            {
+                GetInput(grid, player);
+
+
+            }
+
+        }
+
+        private static void BuildWorld(int[,] grid)
+        {
+
+        }
+
+        private static void GetInput(int[,] grid, object instance)
+        {
+
+            Player player = (Player)instance;
+            grid[player.y, player.x] = 0;
+            int x = player.x;
+            int y = player.y;
+            if (Console.KeyAvailable == true)
+            {
+
+                player.Input = Console.ReadKey().Key.ToString();
+                WriteAt("  ", x * 2, y - 1);
+                WriteAt("  ", x * 2, y);
+            }
+
+
+
+
+
+            switch (player.Input)
+            {
+                case "W":
+
+
+                    break;
+                case "A":
+
+                    x--;
+
+                    break;
+                case "S":
+
+
+                    break;
+                case "D":
+                    x++;
+
+                    break;
+            }
+            player.Input = null;
+            player.x = x;
+            player.y = y;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            WriteAt("██", x * 2, y - 1);
+            WriteAt("██", x * 2, y);
+            Console.ForegroundColor = default;
+            WriteAt("", 110, 0);
+
+
+        }
+        static void Fill_Index(int x, int y, int[,] grid, int id,string[] Blocks)
+        {
+
+            for (int j = 0; j < y; j++)
+            {
+                for (int i = 0; i < x; i++)
+                {
+                    grid[y, x] = id;
+                    WriteAt(Blocks[id], x * 2, y);
+                }
+            }
+        }
+        
+    }
+}
