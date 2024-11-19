380fd51c202240662c757e92feb4bc54ba4dc594
Yharlm <133050108+Yharlm@users.noreply.github.com>
2024-11-18 23:25:49 -1100

    Update Class1.cs

--- Class1.cs
+++ Class1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,31 +8,36 @@ namespace Minecraft
 {
     class Player
     {
+        public string last_key = "";
         public string Input = "";
         public int x = 4;
         public int y = 11;
         public int x_size = 70;
         public int y_size = 34;
+        public bool grounded = false;
     }
 
-    class cordinates : Player 
+    class cordinates : Player
     {
         public int x = 4;
         public int y = 4;
-        
+
 
     }
-    
-    class Game  
+
+    class Game
     {
         public int x_size = 30;
         public int y_size = 13;
 
     }
 
-    class Block_ids
+    class Block_ids(int Id, string texture, ConsoleColor fG, ConsoleColor bG)
     {
-         
+        public int id = Id;
+        public string Texture = texture;
+        public ConsoleColor FG = fG;
+        public ConsoleColor BG = bG;
     }
     //▀▄░█
 }
